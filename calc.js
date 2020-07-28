f24b91948c1157623ff4f8af60c56b55a5adb022
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2020-07-28 17:17:11 +0200

    Grid-Area für Rechnerlayout, Startseite

--- calc.js
+++ calc.js
@@ -3,7 +3,14 @@ var input = document.getElementById('pounds');
 var resultInKg = document.getElementById('result');
 
 button.addEventListener('click', function() {
-    input = parseInt(input.value);
-    resultInKg.textContent = (input / 2.2046).toFixed(2);
+  input = parseFloat(input.value);
+  resultInKg.textContent = (input / 2.2046).toFixed(2);
 
 })
+
+
+/* To-Do-Liste
+- Was tun, wenn Komma eingegeben wird? Umwandeln?
+- Neue Eingabe ohne neu laden zu müssen
+- "Eingabe ändern"-Button?
+*/
