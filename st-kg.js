e9750e96e95d7c20a0b63ef943f0086a6c321618
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2020-07-30 18:43:14 +0200

    st-kg bearbeitet

---
+++ st-kg.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('to-kg-btn');
+var input = document.getElementById('stone');
+var result = document.getElementById('in-kg-result');
+
+button.addEventListener('click', function() {
+
+})
